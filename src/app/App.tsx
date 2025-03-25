f8d33433f81ad9d3475be608f77178e746c07128
YADARAN1 <danil-tkachenko-01@list.ru>
2025-03-25 15:28:50 +0500

    configure svg, png, File loaders and added first UI Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,12 @@ import { AppRoter } from "./providers/router";
 import "./styles/index.scss";
 
 const App = () => {
-	const { theme, toggleTheme } = useTheme();
+	const { theme } = useTheme();
 
 	return (
 		<div className={classNames("app", {}, [theme])}>
 			<Navbar />
 			<AppRoter />
-			<button onClick={toggleTheme}>TOGGLE</button>
 		</div>
 	);
 };
